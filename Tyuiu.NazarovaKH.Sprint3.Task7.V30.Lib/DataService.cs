3b159227cf3f1777ea435ea75d932232c96638e5
Tyuiu.NazarovaKKH@outlook.com <Tyuiu.NazarovaKKH@outlook.com>
2024-11-28 20:27:05 +0300

    Task7

--- Tyuiu.NazarovaKH.Sprint3.Task7.V30.Lib/DataService.cs
+++ Tyuiu.NazarovaKH.Sprint3.Task7.V30.Lib/DataService.cs
@@ -13,16 +13,7 @@ namespace Tyuiu.NazarovaKH.Sprint3.Task7.V30.Lib
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                double denominator = x + 1;
-                if (denominator == 0)
-                {
-                    y = 0;
-                }
-                else
-                {
-                    y = ((5 * x + 2.5) / (Math.Sin(x) + 2)) + 2 * x + 2;
-                    y = Math.Round(y, 2);
-                }
+                y = Math.Round(((5 * x + 2.5) / (Math.Sin(x) + 2)) + 2 * x + 2, 2);
                 valueArray[count] = y;
                 count++;
             }
