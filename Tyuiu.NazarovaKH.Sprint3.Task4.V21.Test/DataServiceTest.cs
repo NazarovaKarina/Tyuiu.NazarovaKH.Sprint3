8a91b3608fe7329ec8f087263ac4b217c588e8a5
Tyuiu.NazarovaKKH@outlook.com <Tyuiu.NazarovaKKH@outlook.com>
2024-11-28 17:15:07 +0300

    Task4

---
+++ Tyuiu.NazarovaKH.Sprint3.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Tyuiu.NazarovaKH.Sprint3.Task4.V21.Lib;
+
+namespace Tyuiu.NazarovaKH.Sprint3.Task4.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = -4.896;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NazarovaKH.Sprint3.Task4.V21/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.NazarovaKH.Sprint3.Task4.V21.Lib;
+
+namespace Tyuiu.NazarovaKH.Sprint3.Task4.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 18;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.Calculate( startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
